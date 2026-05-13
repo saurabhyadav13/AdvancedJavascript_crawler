45c8643f8613f62dc2940395ee47f676f9086294
saurabhyadav13 <90502564+saurabhyadav13@users.noreply.github.com>
2026-05-13 18:17:12 +0530

    Update game.js

--- game.js
+++ game.js
@@ -3,12 +3,7 @@ let TIME_LIMIT = 60;
 
 // define quotes to be used
 let quotes_array = [
-  "Push yourself, because no one else is going to do it for you.",
-  "Failure is the condiment that gives success its flavor.",
-  "Wake up with determination. Go to bed with satisfaction.",
-  "It's going to be hard, but hard does not mean impossible.",
-  "Learning never exhausts the mind.",
-  "The only way to do great work is to love what you do."
+  "
 ];
 
 // selecting required elements
